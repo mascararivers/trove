35f344f2fddb1e1f1e9524690a38a7cc51a12a93
max <hxllfxres@gmail.com>
2025-05-18 10:54:17 +0000

    Added basic player data parsing

--- src/api/player_data.rs
+++ src/api/player_data.rs
@@ -1,33 +1,34 @@
 use crate::errors::Result;
 use serde::Deserialize;
 
-#[derive(Debug, Default, Deserialize)]
-pub struct Stats {
 
-}
-
-#[derive(Default, Deserialize)]
-pub struct Data {
+#[derive(Default, Deserialize, Debug)]
+pub struct Player {
     pub uuid: String,
     pub displayname: String,
-    pub rank: String,
-    #[serde(rename = "packageRank")] pub package_rank: String,
-    #[serde(rename = "newPackageRank")] pub new_package_rank: String,
-    #[serde(rename = "monthlyPackageRank")] pub monthly_package_rank: String,
+    pub rank: Option<String>,
+    #[serde(rename = "packageRank")] pub package_rank: Option<String>,
+    #[serde(rename = "newPackageRank")] pub new_package_rank: Option<String>,
+    #[serde(rename = "monthlyPackageRank")] pub monthly_package_rank: Option<String>,
     #[serde(rename="firstLogin")] pub first_login: i64,
     #[serde(rename = "lastLogin")] pub last_login: i64,
     #[serde(rename = "lastLogout")] pub last_logout: i64,
-    pub stats: Stats,
+}
+
+#[derive(Default, Deserialize, Debug)]
+pub struct Data {
+    success: bool,
+    player: Player,
 }
 
 
-pub async fn retrieve_player_data(player_id: &str, api_key: String, http_client: reqwest::Client) -> Result<Data> {
-
-    let request = http_client.get(format!("https://api.hypixel.net/v2/player/{}", player_id)).bearer_auth(api_key);
+pub async fn retrieve_player_data(player_id: String, api_key: String, http_client: reqwest::Client) -> Result<Data> {
+    let request = http_client.get(format!("https://api.hypixel.net/v2/player?uuid={}", player_id)).header("API-Key", api_key);
     let response = request.send().await?;
 
-    
-    let data: Data = serde_json::from_str(response.text().await?.as_str())?;
+    let text = response.text().await?;
+
+    let data: Data = serde_json::from_str::<Data>(&text)?;
     
     Ok(data)
 }
