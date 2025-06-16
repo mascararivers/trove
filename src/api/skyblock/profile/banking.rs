18687917b344372a822e2d2be5d9f12f977b761a
max <hxllfxres@gmail.com>
2025-06-16 13:43:27 +0100

    updated banking struct

--- src/api/skyblock/profile/banking.rs
+++ src/api/skyblock/profile/banking.rs
@@ -13,8 +13,34 @@ pub struct Banking {
     balance: usize,
     transactions: Vec<BankTransaction>,
 }
+#[derive(Deserialize, Debug)]
+pub struct EssenceStats {
+    current: usize,    
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Essence {
+    #[serde(rename = "WITHER")]
+    wither: EssenceStats,
+    #[serde(rename = "DRAGON")]
+    dragon: EssenceStats,
+    #[serde(rename = "SPIDER")]
+    spider: EssenceStats,
+    #[serde(rename = "UNDEAD")]
+    undead: EssenceStats,
+    #[serde(rename = "DIAMOND")]  
+    diamond: EssenceStats,
+    #[serde(rename = "GOLD")]
+    gold: EssenceStats,
+    #[serde(rename = "ICE")]
+    ice: EssenceStats,
+    #[serde(rename = "CRIMSON")]  
+    crimson: EssenceStats,
+}
 
 #[derive(Deserialize, Debug)]
 pub struct Currencies {
     coin_purse: usize,
+    motes_purse: usize,
+    essence: Essence,
 }
